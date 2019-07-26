18c2234f634d281e4900246d265410aa3db887c6
ZiYi <sheeploveart@gmail.com>
2019-07-26 16:17:57 +0800

    List-item Draggable

--- components/EditHome.vue
+++ components/EditHome.vue
@@ -1,5 +1,56 @@
  <template>
  <div class="edit-home">
+<div class="row">
+    <!-- <div class="col-2">
+      <div class="form-group">
+        <div
+          class="btn-group-vertical buttons"
+          role="group"
+          aria-label="Basic example"
+        >
+          <button class="btn btn-secondary" @click="add">Add</button>
+          <button class="btn btn-secondary" @click="replace">Replace</button>
+        </div>
+
+        <div class="form-check">
+          <input
+            id="disabled"
+            type="checkbox"
+            v-model="enabled"
+            class="form-check-input"
+          />
+          <label class="form-check-label" for="disabled">DnD enabled</label>
+        </div>
+      </div>
+    </div> -->
+
+    <div>
+      <!-- <h3>Draggable {{ draggingInfo }}</h3> -->
+
+      <!-- <draggable
+        :list="list"
+        :disabled="!enabled"
+        class="list-group"
+        ghost-class="ghost"
+        :move="checkMove"
+        @start="dragging = true"
+        @end="dragging = false"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list"
+          :key="element.name"
+        >
+          {{ element.name }}
+        </div>
+      </draggable> -->
+    </div>
+
+    <!-- <rawDisplayer class="col-3" :value="list" title="List" /> -->
+  </div>
+
+
+
     <transition name="fade">
     <div class="side-content main" v-if="isLayer1">
       <el-select v-model="page" placeholder="請選擇頁面" @change="mainSelect">
@@ -26,12 +77,30 @@
                   </el-button>
                 </li>
                 <li>頁尾導航列</li>
-                <li>橫幅</li>
-                <li>輪播特效</li>
-                <li>跑馬燈</li>
-                <li>內容</li>
+                <draggable
+                  :list="moveList"
+                  :disabled="!enabled"
+                  class="list-group"
+                  ghost-class="ghost"
+                  :move="checkMove"
+                  @start="dragging = true"
+                  @end="dragging = false"
+                >
+                  <li
+                    class="list-group-item"
+                    v-for="element in moveList"
+                    :key="element.name"
+                  >
+                    {{ element.name }}
+                  </li>
+                </draggable>
                 <li>頁尾</li>
-                <li>新增區塊</li>
+                <li>
+                  <span>新增區塊</span>
+                   <el-button round class="btn-setting" @click="add">
+                    <i class="el-icon-plus"></i>
+                  </el-button>
+                </li>
               </ul>
             </div>
           </el-tab-pane>
@@ -98,18 +167,30 @@
 </template>
 <script>
 import { mapState, mapGetters } from "vuex";
+import draggable from "vuedraggable";
+let id = 1;
 export default {
+  display: "Simple",
+  order: 0,
   components: {
+    draggable
   },
   data() {
     return {
       activeName: 'first',
-    //   activeVersion: 'web',
       page: '',
-    //   value2: '',
       isPreview: true,
       isLayer1: true,
-      isLayer2: false
+      isLayer2: false,
+      //drag
+      enabled: true,
+      moveList: [
+        { name: "橫幅", id: 0 },
+        { name: "輪播特效", id: 1 },
+        { name: "跑馬燈", id: 2 },
+        { name: "內容", id: 3 }
+      ],
+      dragging: false
     }
   },
   computed: {
@@ -129,6 +210,10 @@ export default {
       "isAgentReg",
       "isForgot"
     ]),
+    //drag
+    draggingInfo() {
+      return this.dragging ? "under drag" : "";
+    }
   },
   methods: {
     showSetting() {
@@ -141,18 +226,43 @@ export default {
         this.isLayer2 = false;
       }
     },
-    // toggle() {
-    //   this.isPreview = !this.isPreview;
-    // },
     mainSelect(val) {
       this.$store.commit("main/updateSelect", { value: val });
     },
-    handleClick() {
-      
+    handleClick() {},
+    //drag
+    add: function() {
+      this.moveList.push({ name: "新區塊" + id, id: id++ });
+    },
+    replace: function() {
+      this.moveList = [{ name: "移除區塊", id: id++ }];
+    },
+    checkMove: function(e) {
+      window.console.log("Future index: " + e.draggedContext.futureIndex);
     }
-  },
+  }
 }
- </script>
- 
+</script>
+<style lang="scss" scoped>
+.buttons {
+  margin-top: 35px;
+}
+.ghost {
+  opacity: .5;
+  background: #c8ebfb;
+}
+.list-group {
+  margin: 10px 0;
+  box-shadow: 0 0 1px rgba(0,0,0, .2);
+  background: #fff;
+}
+
+// .list-group-item {
+//   border: 1px solid #ddd;
+//   background: #fff;
+//   &:hover{border-right: 4px solid rgb(55, 88, 164);}
+// }
+</style>
+
  
  
